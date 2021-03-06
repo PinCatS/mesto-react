ec7a3bf6b0a7285ea26fc3bc9e7b386100ff8e1c
Sergey Li <pincats@gmail.com>
2021-03-06 15:25:43 +0300

    feat: Add close popup functionality

--- src/components/App.js
+++ src/components/App.js
@@ -23,6 +23,22 @@ function App() {
     setIsEditAvatarPopupOpen(true);
   }
 
+  const closeAllPopups = (popupName) => {
+    switch (popupName) {
+      case 'edit-profile':
+          setIsEditProfilePopupOpen(false);
+          break;
+      case 'add-card':
+          setIsAddPlacePopupOpen(false);
+          break;
+      case 'update-avatar':
+          setIsEditAvatarPopupOpen(false);
+          break;
+      default:
+          console.error('closeAllPopups: Unexpected popup name. Check the logic.');
+    }
+  }
+
 
   return (
     <div className="App page">
@@ -30,54 +46,66 @@ function App() {
         <Main onEditProfile={onEditProfile} onAddPlace={onAddPlace} onEditAvatar={onEditAvatar} />
         <Footer />
 
-        <PopupWithForm name="edit-profile" title="Редактировать профиль" isOpen={isEditProfilePopupOpen}>
-          <input
-            type="text"
-            className="form-input popup__input popup__input_name_name"
-            name="profile-name"
-            placeholder="Имя профиля"
-            minLength="2" maxLength="40"
-            required /> 
-          <span className="popup__input-error popup__input-error_name_profile-name"></span>
-          <input
-            type="text"
-            className="form-input popup__input popup__input_name_activity"
-            name="profile-activity"
-            placeholder="Деятельность"
-            minLength="2" maxLength="200"
-            required />
-          <span className="popup__input-error popup__input-error_name_profile-activity"></span>
-          <button type="submit" className="button popup__save-button">Сохранить</button>
+        <PopupWithForm
+          name="edit-profile"
+          title="Редактировать профиль"
+          isOpen={isEditProfilePopupOpen}
+          onClose={closeAllPopups}>
+            <input
+              type="text"
+              className="form-input popup__input popup__input_name_name"
+              name="profile-name"
+              placeholder="Имя профиля"
+              minLength="2" maxLength="40"
+              required /> 
+            <span className="popup__input-error popup__input-error_name_profile-name"></span>
+            <input
+              type="text"
+              className="form-input popup__input popup__input_name_activity"
+              name="profile-activity"
+              placeholder="Деятельность"
+              minLength="2" maxLength="200"
+              required />
+            <span className="popup__input-error popup__input-error_name_profile-activity"></span>
+            <button type="submit" className="button popup__save-button">Сохранить</button>
         </PopupWithForm>
 
-        <PopupWithForm name="add-card" title="Новое место" isOpen={isAddPlacePopupOpen}>
-          <input
-                  type="text"
-                  className="form-input popup__input popup__input_name_place-name"
-                  name="place-name"
-                  placeholder="Название"
-                  minLength="2" maxLength="30"
-                  required />
-                <span className="popup__input-error popup__input-error_name_place-name"></span>
-                <input
-                  type="url"
-                  className="form-input popup__input popup__input_name_place-image-url"
-                  name="place-image-url"
-                  placeholder="Ссылка на картинку"
-                  required />
-                <span className="popup__input-error popup__input-error_name_place-image-url"></span>
-                <button type="submit" className="button popup__save-button">Создать</button>
+        <PopupWithForm
+          name="add-card"
+          title="Новое место"
+          isOpen={isAddPlacePopupOpen}
+          onClose={closeAllPopups}>
+            <input
+                    type="text"
+                    className="form-input popup__input popup__input_name_place-name"
+                    name="place-name"
+                    placeholder="Название"
+                    minLength="2" maxLength="30"
+                    required />
+                  <span className="popup__input-error popup__input-error_name_place-name"></span>
+                  <input
+                    type="url"
+                    className="form-input popup__input popup__input_name_place-image-url"
+                    name="place-image-url"
+                    placeholder="Ссылка на картинку"
+                    required />
+                  <span className="popup__input-error popup__input-error_name_place-image-url"></span>
+                  <button type="submit" className="button popup__save-button">Создать</button>
         </PopupWithForm>
 
-        <PopupWithForm name="update-avatar" title="Обновить аватар" isOpen={isEditAvatarPopupOpen}>
-          <input
-                  type="url"
-                  className="form-input popup__input popup__input_name_avatar-link"
-                  name="avatar-link"
-                  placeholder="https://somewebsite.com/someimage.jpg"
-                  required />
-                <span className="popup__input-error popup__input-error_name_avatar-link"></span>
-                <button type="submit" className="button popup__save-button">Сохранить</button>       
+        <PopupWithForm
+          name="update-avatar"
+          title="Обновить аватар"
+          isOpen={isEditAvatarPopupOpen}
+          onClose={closeAllPopups}>
+            <input
+                    type="url"
+                    className="form-input popup__input popup__input_name_avatar-link"
+                    name="avatar-link"
+                    placeholder="https://somewebsite.com/someimage.jpg"
+                    required />
+                  <span className="popup__input-error popup__input-error_name_avatar-link"></span>
+                  <button type="submit" className="button popup__save-button">Сохранить</button>       
         </PopupWithForm>
 
         <PopupWithForm name="remove-card" title="Вы уверены?">
