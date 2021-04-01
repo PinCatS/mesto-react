1dd7f401641d01ceb1d8d73ff2996381060ad932
Sergey Li <pincats@gmail.com>
2021-04-01 11:21:30 +0300

    fix: Return markup without popup_opened in case there is no card or link

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -2,19 +2,17 @@ import '../index.css';
 
 function ImagePopup({card, onClose}) {
     return (
-      card && (
-        <div className="popup image-popup popup_opened">
-          <figure className="popup__container popup__container_size_small image-popup__container">
-            <button
-                type="button"
-                aria-label="Закрыть картинку"
-                className="button popup__close-button popup__close-button_location_topright"
-                onClick={onClose}></button>
-            <img className="image-popup__image" src={card.link} alt={card.name} />
-            <figcaption className="image-popup__caption">{card.name}</figcaption>
-          </figure>
-        </div>
-      )
+      <div className={`popup image-popup ${card?.link && 'popup_opened'}`}>
+        <figure className="popup__container popup__container_size_small image-popup__container">
+          <button
+              type="button"
+              aria-label="Закрыть картинку"
+              className="button popup__close-button popup__close-button_location_topright"
+              onClick={onClose}></button>
+          <img className="image-popup__image" src={card?.link} alt={card?.name} />
+          <figcaption className="image-popup__caption">{card?.name}</figcaption>
+        </figure>
+      </div>
     );
 }
 
